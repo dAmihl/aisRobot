175851ff977cd1f79a28ae0630310e347e654f34
co0k <gerald.kofler@student.uibk.ac.at>
2015-04-01 13:28:07 +0200

    SensorData

--- app/src/main/java/com/example/damihl/robotmove/obstacleavoidance/ObstacleAvoidManager.java
+++ app/src/main/java/com/example/damihl/robotmove/obstacleavoidance/ObstacleAvoidManager.java
@@ -21,12 +21,25 @@ public class ObstacleAvoidManager {
         String sensorData = this.controlManager.getSensorData();
 
         String arr[] = sensorData.split(" ");
-        int value1 = Integer.parseInt(arr[arr.length -2].substring(2), 16);
-        int value2 = Integer.parseInt(arr[arr.length -3].substring(2), 16);
-        int value3 = Integer.parseInt(arr[arr.length -4].substring(2), 16);
+        //int value1 = Integer.parseInt(arr[arr.length -2].substring(2), 16);
+        int valueRight = Integer.parseInt(arr[arr.length -5].substring(2), 16);
+        int valueMid = Integer.parseInt(arr[arr.length -4].substring(2), 16);
+        int valueLeft = Integer.parseInt(arr[arr.length -6].substring(2), 16);
         application.threadSafeDebugOutput("SensorData: "+sensorData);
-        application.threadSafeDebugOutput("Value Sensor 1: "+value1+" Sensor 2: "+value2+" Sensor 3:"+value3);
+        application.threadSafeDebugOutput("Value Sensor 1 Left: "+valueLeft+" Sensor 2 Mid: "+valueMid+" Sensor 3 Right:"+valueRight);
 
+        if(valueLeft <= 20){
+            application.threadSafeDebugOutput("Obstacle Sensor Left "+valueLeft);
+            return true;
+        }
+        if(valueMid <= 10){
+            application.threadSafeDebugOutput("Obstacle Sensor Mid"+valueMid);
+            return true;
+        }
+        if(valueRight <= 20){
+            application.threadSafeDebugOutput("Obstacle Sensor Right "+valueRight);
+            return true;
+        }
         /*if(value == 15 ){
             return true;
         }
@@ -34,8 +47,6 @@ public class ObstacleAvoidManager {
         {
             return true;
         }*/
-      application.threadSafeDebugOutput("SensorData: "+sensorData);
-      application.threadSafeSensorDataOutput(arr);
         /*if( insert Obstacle checking algorithm based on sensor data here)
         *   return true
         *else */
